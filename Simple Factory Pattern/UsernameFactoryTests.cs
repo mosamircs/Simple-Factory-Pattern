4a79f210dfb1d7466d5bd3055941257fd639f102
mosamircs <mosamircs@gmail.com>
2021-07-21 07:25:03 +0200

    adding xUnit test

---
+++ Simple Factory Pattern/UsernameFactoryTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Simple_Factory_Pattern
+{
+    public class UsernameFactoryTests
+    {
+        private UsernameFactory _factory;
+
+        public UsernameFactoryTests()
+        {
+            _factory = new UsernameFactory();
+        }
+
+        [Fact]
+        public void ShouldGetFirstNameFirst()
+        {
+            //arrange
+            var user = "Gary Woodfine";
+
+            //act
+            var username = _factory.GetUserName(user);
+
+            //assert
+            Assert.Equal("Gary", username.FirstName);
+            Assert.Equal("Woodfine", username.LastName);
+
+        }
+
+        [Fact]
+        public void ShouldGetLastNameFirst()
+        {
+            //arrange
+            var user = "Woodfine, Gary";
+
+            //act
+            var username = _factory.GetUserName(user);
+
+            //assert
+            Assert.Equal("Gary", username.FirstName);
+            Assert.Equal("Woodfine", username.LastName);
+
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    public class FirstNameFirstTest
+    {
+        [Fact]
+        public void ShouldSplitStringOnSpace()
+        {
+            var theName = "Gary Woodfine";
+            var splitname = new FirstNameFirst(theName);
+
+            Assert.Equal("Gary", splitname.FirstName);
+            Assert.Equal("Woodfine", splitname.LastName);
+
+        }
+
+        [Fact]
+        public void FailSplitOnStringOnComma()
+        {
+            var theName = "Gary,Woodfine";
+            var splitname = new FirstNameFirst(theName);
+
+            Assert.Null(splitname.FirstName);
+            Assert.Null(splitname.LastName);
+
+        }
+
+    }
+}
