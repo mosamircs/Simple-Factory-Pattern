0470a31b4b43de071d91eb5dc2e91f3664322e97
mosamircs <mosamircs@gmail.com>
2021-07-21 07:11:24 +0200

    Build a Simple Factory Pattern

---
+++ Simple Factory Pattern/UsernameFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Factory_Pattern
+{
+    class UsernameFactory
+    {
+        public UserName GetUserName(string name)
+        {
+            if (name.Contains(",")) return new LastNameFirst(name);
+
+            return new FirstNameFirst(name);
+        }
+    }
+}
